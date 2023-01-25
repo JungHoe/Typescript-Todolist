012d77bc63f7eac918dce8bf1166b8e4f6ea79c6
JungHoe <junghwi0603@gmail.com>
2023-01-25 11:03:38 +0900

    Todolist 테마 적용
     * dark , light 테마 선택에 따른 css 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,83 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useState, useCallback } from "react";
+import {
+  ConfigProvider,
+  theme,
+  Layout,
+  Col,
+  Row,
+  Switch,
+  Input,
+  Button,
+  Divider,
+} from "antd";
 import "./App.css";
+const { Header, Content, Footer } = Layout;
+const { darkAlgorithm, defaultAlgorithm } = theme;
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  );
+interface TodoItem {
+  id: number;
+  text: string;
+  status: string;
 }
+
+const App: React.FC = () => {
+  const [isDark, setIsDark] = useState(true);
+  const [text, setText] = useState("");
+  const [list, setList] = useState<TodoItem[]>([
+    { id: 1, text: "테스트 메세지", status: "doing" },
+  ]);
+
+  const onChangeText = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  }, []);
+  const onSubmitTodo = () => {
+    setList([...list, { id: new Date().getTime(), text, status: "doing" }]);
+    setText("");
+  };
+  const themeName = isDark ? "dark" : "light";
+  return (
+    <ConfigProvider
+      theme={{
+        algorithm: isDark ? darkAlgorithm : defaultAlgorithm,
+        token: {
+          colorBgBase: isDark ? "#262626" : "#fff",
+        },
+      }}
+    >
+      <Layout id="layout-wrapper" className={themeName}>
+        <Header className="ta-c">
+          <h2>Todo-List</h2>
+          <div className="mode-container">
+            <Switch
+              checked={!isDark}
+              checkedChildren="Light"
+              unCheckedChildren="Dark"
+              onClick={() => setIsDark(!isDark)}
+            />
+          </div>
+        </Header>
+        <Layout id="main-app">
+          <Content>
+            <Row className="write-form">
+              <Col span={20}>
+                <Input value={text} onChange={onChangeText}></Input>
+              </Col>
+              <Col offset={1} span={3}>
+                <Button onClick={onSubmitTodo}>작성</Button>
+              </Col>
+            </Row>
+            <Divider />
+            <ul>
+              {list?.map((item) => {
+                return <li key={item.id}>{item.text}</li>;
+              })}
+            </ul>
+          </Content>
+        </Layout>
+        <Footer className="ta-c">footer</Footer>
+      </Layout>
+    </ConfigProvider>
+  );
+};
 
 export default App;

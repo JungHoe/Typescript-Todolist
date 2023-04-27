1db673c1e6f4aa422c9674b4b31a95bc8fa54ef7
JungHoe <junghwi0603@gmail.com>
2023-04-27 11:06:31 +0900

    test: header 테스트 명시적으로 변경
    * expect toBeInTheDocument 추가

--- src/components/__tests__/TodoItem/header.test.tsx
+++ src/components/__tests__/TodoItem/header.test.tsx
@@ -10,20 +10,21 @@ describe("<TodoItemHeader />", () => {
     render(<Todo />);
     const root = screen.getByTestId("todo-item-header");
     expect(root).toHaveStyle({ "background-color": "#a15767" });
-    screen.getByText(StatusLabel.todo);
+
+    expect(screen.getByText(StatusLabel.todo)).toBeInTheDocument();
   });
 
   it("Doing 텍스트 및 배경 확인", () => {
     render(<Doing />);
     const root = screen.getByTestId("todo-item-header");
     expect(root).toHaveStyle({ "background-color": "#c57f40" });
-    screen.getByText(StatusLabel.doing);
+    expect(screen.getByText(StatusLabel.doing)).toBeInTheDocument();
   });
 
   it("Done 텍스트 및 배경 확인", () => {
     render(<Done />);
     const root = screen.getByTestId("todo-item-header");
     expect(root).toHaveStyle({ "background-color": "#63ad27" });
-    screen.getByText(StatusLabel.done);
+    expect(screen.getByText(StatusLabel.done)).toBeInTheDocument();
   });
 });
